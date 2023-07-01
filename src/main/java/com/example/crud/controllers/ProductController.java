fb38f90917b5e6e2df4913e6415d1ccb585e178f
ConstantinoRafael <rafael.constantino89@gmail.com>
2023-07-01 06:14:56 -0300

    feat: delete route

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -4,7 +4,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -51,4 +53,11 @@ public class ProductController {
 
         return ResponseEntity.notFound().build();
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteProduct(@PathVariable String id) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
